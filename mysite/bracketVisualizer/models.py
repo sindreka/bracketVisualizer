b958430015aad4c6a48d0c700491b190cb097125
schpere <harald.sperre@gmail.com>
2016-11-12 14:43:44 +0100

    Backend stuff works!

--- mysite/bracketVisualizer/models.py
+++ mysite/bracketVisualizer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+def getInfoFromLine(line):
+    info = line.split(" defeats ")
+    info = [info[0]] + info[1].split(' with ')
+    info[2] = info[2][0:info[2].find('%')]
+    return info
+
+def getInfoFromPost(text):
+    info = []
+    for line in text:
+        info.append(getInfoFromLine(line))
+    return info
+
