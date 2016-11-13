4030968e32b3e5bed2f212c60ad25dd2209451c3
schpere <harald.sperre@gmail.com>
2016-11-13 16:40:00 +0100

    Added function to get results

--- mysite/bracketVisualizer/models.py
+++ mysite/bracketVisualizer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import json
+from urllib import request 
 
 # Create your models here.
 
@@ -6,3 +8,24 @@ class bracketModel(models.Model):
 	title = models.CharField(max_length = 200)
 	post = 	models.TextField()
 	
+
+class AppURLopener(request.FancyURLopener):
+    version = "User-Agent:bracketVisualizer:v0.2 (by /u/schpere)"
+ 
+
+def getResults(batchNumber):
+    batchNumber = str(batchNumber)
+    request._urlopener = AppURLopener
+    url = "http://reddit.com/r/mtgbracket.json"
+    source = urllib.urlopen(url)
+    posts = json.load(source)["data"]["children"]
+    for post in posts:
+        if post['data']['title'] == "Batch " + batchNumber + " results":
+            url = "http://reddit.com" + post['data']['permalink']
+            break
+    source = urllib.urlopen(url + ".json")
+    comments = json.load(source)[1]['data']['children']
+    for comment in comments:
+        if comment['data']['body'].count('%') >= 32:
+            post = comment['data']['body']
+            return post[post.find("*")+2:].split("\n* ")
