ccbb07bd308c9a24df75c7cdac55773e5f412fdd
Harald Sperre <harald.sperre@gmail.com>
2017-01-17 18:31:20 +0100

    I try to draw dice, but updating game area is weird.

--- lolturbine/functions.js
+++ lolturbine/functions.js
@@ -2,6 +2,9 @@ var myBackground;
 var nations = [];
 var attacker;
 var reinforcer;
+var clickRadius = 40;
+var mouseOffsetX = 15;
+var mouseOffsetY = - 10;
 
 var player = {
     color : "red",
@@ -12,17 +15,18 @@ var player = {
 function startGame() {
     myBackground = new map(640,400,"maps/first.png");
 
-    nations[0] = new nation(3,"red",[1,2],78,99);
-    nations[1] = new nation(3,"blue",[0,2,3],212,221);
-    nations[2] = new nation(3,"green",[0,1,3],424,160);
-    nations[3] = new nation(3,"orange",[1,2],368,313);
+    nations[0] = new nation(0,3,"red",[1,2],78,99);
+    nations[1] = new nation(1,3,"blue",[0,2,3],212,221);
+    nations[2] = new nation(2,3,"red",[0,1,3],424,160);
+    nations[3] = new nation(3,3,"orange",[1,2],368,313);
 
 
     myGameArea.start();
 
 }
 
-function nation(troops,owner,borders,x,y) {
+function nation(id,troops,owner,borders,x,y) {
+    this.id = id;
     this.troops = troops;
     this.owner = owner;
     this.borders = borders;
@@ -33,6 +37,7 @@ function nation(troops,owner,borders,x,y) {
         numDefenders = Math.min(2,victim.troops);
         atk = dieRoll(numAttackers);
         def = dieRoll(numDefenders);
+        drawDice( atk, def, victim.color );
         if (atk[0] > def[0]) { victim.troops--; }
         else { this.troops--; }
         if (numAttackers > 1 && numDefenders > 1) {
@@ -41,8 +46,8 @@ function nation(troops,owner,borders,x,y) {
         }
         if (victim.troops == 0){
             victim.owner = this.owner;
-            victim.troops = 1;
-            this.troops--;
+            victim.troops = this.troops - 1;
+            this.troops = 1;
         }
     }
 }
@@ -51,10 +56,10 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 640;
-        this.canvas.height = 400;
+        this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 1);
         window.addEventListener('mousedown', function (e) {
             myGameArea.x = e.pageX;
             myGameArea.y = e.pageY;
@@ -73,6 +78,18 @@ function updateGameArea() {
     myGameArea.clear();
     myBackground.update();
 
+    if ( attacker ) {
+        ctx.lineWidth=10;
+        ctx.globalAlpha = .50;
+        ctx.strokeStyle = player.color;
+        ctx.beginPath();
+        ctx.arc( attacker.x + mouseOffsetX, attacker.y + mouseOffsetY, clickRadius, 0, 2 * Math.PI );
+        ctx.stroke();
+        ctx.globalAlpha = 1;
+    }
+
+    drawDice([1,2,3],[4,5,6], "orange" );
+
     for (i = 0; i < nations.length ; i++) { 
         drawText(nations[i].troops,nations[i].owner,nations[i].x,nations[i].y);
     }
@@ -90,7 +107,7 @@ function updateGameArea() {
                 console.log("Who is attacking?");
                 attacker = clicked;
             }
-            else if ( clicked.owner != player.color && attacker ) {
+            else if ( clicked.owner != player.color && attacker && ~attacker.borders.indexOf(clicked.id) ) {
                 console.log("Attack!");
                 attacker.attack(clicked);
                 if ( attacker.troops < 2 ) {
@@ -136,7 +153,7 @@ function dist(x1,y1,x2,y2) {
 function click() {
     if ( myGameArea.x ) {
         for ( i = 0 ; i < nations.length ; i++ ) {
-            if ( dist( nations[i].x,nations[i].y,myGameArea.x - 15,myGameArea.y ) < 50 ** 2 ) { 
+            if ( dist( nations[i].x,nations[i].y,myGameArea.x - mouseOffsetX,myGameArea.y - mouseOffsetY ) < clickRadius ** 2 ) { 
                 myGameArea.x = false;
                 myGameArea.y = false;
                 return nations[i] }
@@ -144,3 +161,45 @@ function click() {
     }
     return false;
 }
+
+function drawDice( atk, def, victim ) {
+    var x = 10;
+    var y = 5;
+    var dieSize = 30;
+    for (i = 0 ; i < atk.length ; i++ ) {
+        drawDie( atk[i], x, y, dieSize, player.color );
+        x += dieSize + 3;
+    }
+    x += 15;
+    for (i = 0 ; i < def.length ; i++ ) {
+        drawDie( def[i], x, y, dieSize, victim );
+        x += dieSize + 3;
+    }
+}
+
+function drawDie( num, x, y, dieSize, color ) {
+    var dotSize = 2;
+    ctx.fillStyle = color;
+    ctx.rect(x, y, dieSize, dieSize);
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    if ( num == 1 || num == 3 || num == 5 ) {
+        ctx.rect( x + dieSize / 2 - dotSize / 2, y + dieSize / 2 - dotSize / 2, dotSize, dotSize );
+    }
+    if ( num == 2 || num == 3 ) {
+        ctx.rect( x + dieSize / 3 - dotSize / 2, y + dieSize / 3 - dotSize / 2, dotSize, dotSize );
+        ctx.rect( x + 2 * dieSize / 3 - dotSize / 2, y + 2 * dieSize / 3 - dotSize / 2, dotSize, dotSize );
+    }
+    if ( num == 4 || num == 5 || num == 6 ) {
+        ctx.rect( x + dieSize / 3 - dotSize / 2, y + dieSize / 3 - dotSize / 2, dotSize, dotSize );
+        ctx.rect( x + dieSize / 3 - dotSize / 2, y + 2 * dieSize / 3 - dotSize / 2, dotSize, dotSize );
+        ctx.rect( x + 2 * dieSize / 3 - dotSize / 2, y + dieSize / 3 - dotSize / 2, dotSize, dotSize );
+        ctx.rect( x + 2 * dieSize / 3 - dotSize / 2, y + 2 * dieSize / 3 - dotSize / 2, dotSize, dotSize );
+    }
+    if ( num == 6 ) {
+        ctx.rect( x + dieSize / 3 - dotSize / 2, y + dieSize / 2 - dotSize / 2, dotSize, dotSize );
+        ctx.rect( x + 2 * dieSize / 3 - dotSize / 2, y + dieSize / 2 - dotSize / 2, dotSize, dotSize );
+    }
+    ctx.stroke();
+}
+
